af1d6eacba718be9e04f32454446dae3147f5222
Soyeb Sadat <Snsadat04@gmail.com>
2024-07-17 20:00:51 -0400

    Initial Commit

---
+++ GamePlan/GamePlanApp.swift
@@ -0,0 +1,17 @@
+//
+//  GamePlanApp.swift
+//  GamePlan
+//
+//  Created by Soyeb Sadat on 7/17/24.
+//
+
+import SwiftUI
+
+@main
+struct GamePlanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
